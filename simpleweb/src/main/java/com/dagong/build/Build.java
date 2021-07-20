a329aa92f748b647c8b11e0337452e814b1acc58
KING_OF_FIRE <>
2021-07-20 09:55:56 +0800

    build的增删改查后端jdbc以及api

---
+++ simpleweb/src/main/java/com/dagong/build/Build.java
@@ -0,0 +1,28 @@
+package com.dagong.build;
+
+import lombok.Data;
+
+import java.sql.Date;
+
+@Data
+public class Build {
+    private int build_id;
+    private String build_name;
+    private int build_floornum;
+    private int build_housenum;
+    private Date build_time;
+    private int build_accept;
+    private String build_layout;
+
+    public Build(int build_id, String build_name,
+                 int build_floornum, int build_housenum,
+                 Date build_time, int build_accept, String build_layout) {
+        this.build_id = build_id;
+        this.build_name = build_name;
+        this.build_floornum = build_floornum;
+        this.build_housenum = build_housenum;
+        this.build_time = build_time;
+        this.build_accept = build_accept;
+        this.build_layout = build_layout;
+    }
+}
---
+++ simpleweb/src/main/java/com/dagong/build/api/buildupdate.java
@@ -0,0 +1,105 @@
+package com.dagong.build.api;
+
+import cn.hutool.json.JSONUtil;
+import com.dagong.build.Build;
+import com.dagong.build.Jdbc;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+
+@WebServlet(name = "buildupdate", urlPatterns = "/build/update")
+public class buildupdate extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        this.doHttp(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        this.doHttp(req, resp);
+    }
+
+    private void doHttp(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        int res = 0;
+        boolean bl = true;
+        String json = null;
+        String sname = null;
+        int sfloornum = -1;
+        int shousenum = -1;
+        Date sdate = null;
+        int saccept = -1;
+        String slayout = null;
+        String dname = null;
+        int dfloornum = -1;
+        int dhousenum = -1;
+        Date ddate = null;
+        int daccept = -1;
+        String dlayout = null;
+        sname = req.getParameter("sname");
+        if(req.getParameter("sfloornum")!=null) {
+            try{
+                sfloornum = Integer.valueOf(req.getParameter("sfloornum"));
+                dfloornum = Integer.valueOf(req.getParameter("dfloornum"));
+            }catch (Exception e)
+            {
+                System.out.println("something wrong with floornum");
+                bl=false;
+            }
+        }
+        if(req.getParameter("shousenum")!=null) {
+            try{
+                shousenum = Integer.valueOf(req.getParameter("shousenum"));
+                dhousenum = Integer.valueOf(req.getParameter("dhousenum"));
+            }catch (Exception e)
+            {
+                System.out.println("something wrong with housenum");
+                bl=false;
+            }
+        }
+        if(req.getParameter("sdate")!=null) {
+            try {
+                sdate = Date.valueOf(req.getParameter("sdate"));
+                ddate = Date.valueOf(req.getParameter("ddate"));
+            } catch (Exception e) {
+                System.out.println("something wrong with date");
+                bl = false;
+            }
+        }
+        if(req.getParameter("saccept")!=null) {
+            try {
+                saccept = Integer.valueOf(req.getParameter("saccept"));
+                daccept = Integer.valueOf(req.getParameter("daccept"));
+            } catch (Exception e) {
+                System.out.println("something wrong with accept");
+                bl = false;
+            }
+        }
+        slayout = req.getParameter("slayout");
+        dlayout = req.getParameter("dlayout");
+
+        if(bl!=false) {
+            Jdbc jdbc = new Jdbc();
+            Build build1 = new Build(0, sname, sfloornum,
+                    shousenum, sdate, saccept, slayout);
+            Build build2 = new Build(0, dname, dfloornum,
+                    dhousenum, ddate, daccept, dlayout);
+            res=jdbc.updateBuild(build1,build2);
+            json = JSONUtil.toJsonStr("update suceess:"+res);
+        }else {
+            json = JSONUtil.toJsonStr("something wrong with your input");
+        }
+        resp.setContentType("application/json;charset=utf-8");
+        PrintWriter pw = resp.getWriter();
+        pw.write(json);
+        pw.flush();
+    }
+}
